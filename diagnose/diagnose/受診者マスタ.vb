dc7f70663937d4375435609aaa703e054ac0c5fe
DESKTOP-OJIT7DO\yoshi <yoshitaka7144@gmail.com>
2019-04-22 18:33:25 +0900

    受診者マスタ：受診者データ表示まで作成

--- diagnose/diagnose/受診者マスタ.vb
+++ diagnose/diagnose/受診者マスタ.vb
@@ -163,7 +163,7 @@ Public Class 受診者マスタ
         Dim cnn As New ADODB.Connection
         cnn.Open(topForm.DB_Diagnose)
         Dim rs As New ADODB.Recordset
-        Dim sql As String = "select Nam, Kana, Sex, Birth, Int((Format(NOW(),'YYYYMMDD')-Format(Birth, 'YYYYMMDD'))/10000) as Age, Tel, Post, Jyu, Tan, Text from UsrM where Ind = '" & ind & "' order by Kana"
+        Dim sql As String = "select UK1.Nam, UK1.Kana, UK1.Sex, UK1.Birth, UK1.Age, UK1.MaxYmdB, KK2.MaxYmdA, UK1.D72, UK1.D73, UK1.D74, KK2.D76, KK2.D77, KK2.D78, KK2.D79, KK2.D80, UK1.Tel, UK1.Post, UK1.Jyu, UK1.Tan, UK1.Text from (select U.Nam, U.Kana, U.Sex, U.Birth, Int((Format(NOW(),'YYYYMMDD')-Format(U.Birth, 'YYYYMMDD'))/10000) as Age, U.Tel, U.Post, U.Jyu, U.Tan, U.Text, K1.MaxYmdB, KK1.D72, KK1.D73, KK1.D74 from UsrM as U left join (select K1.Kana, K1.MaxYmdB, Ken1.D72, Ken1.D73, Ken1.D74 from (select Kana, Max(Ymd) as MaxYmdB from Ken1 group by Kana) as K1 inner join Ken1 On K1.Kana = Ken1.Kana and K1.MaxYmdB = Ken1.Ymd) as KK1 On U.Kana = KK1.Kana where U.Ind = '" & ind & "') as UK1 left join (select K2.Kana, K2.MaxYmdA, Ken2.D76, Ken2.D77, Ken2.D78, Ken2.D79, Ken2.D80 from (select Kana, Max(Ymd) as MaxYmdA from Ken2 group by Kana) as K2 inner join Ken2 On K2.Kana = Ken2.Kana and K2.MaxYmdA = Ken2.Ymd) as KK2 On UK1.Kana = KK2.Kana order by UK1.Kana"
         rs.Open(sql, cnn, ADODB.CursorTypeEnum.adOpenForwardOnly, ADODB.LockTypeEnum.adLockReadOnly)
         Dim da As OleDbDataAdapter = New OleDbDataAdapter()
         Dim ds As DataSet = New DataSet()
@@ -175,9 +175,80 @@ Public Class 受診者マスタ
         dt.Columns.Add("LastDate", GetType(String)) '実施日
         dt.Columns.Add("Decision", GetType(String)) '判定
         For Each row As DataRow In dt.Rows
+            '名簿列デフォルトでチェック有
             row("List") = True
-            row("LastDate") = ""
-            row("Decision") = ""
+            '実施日(B5とA4の実施日で比較し最新のを表示)
+            Dim maxB As String = Util.checkDBNullValue(row("MaxYmdB"))
+            Dim maxA As String = Util.checkDBNullValue(row("MaxYmdA"))
+            If maxB = "" AndAlso maxA = "" Then
+                row("LastDate") = ""
+            Else
+                row("LastDate") = If(maxB >= maxA, Util.convADStrToWarekiStr(maxB) & "  B5", Util.convADStrToWarekiStr(maxA) & "  A4")
+            End If
+            '判定
+            Dim decisionList As New List(Of String)
+            If maxB >= maxA Then
+                'B5のデータの"要"が含まれてる文字取得
+                Dim d72 As String = Util.checkDBNullValue(row("D72")).Replace("　", " ")
+                Dim d73 As String = Util.checkDBNullValue(row("D73")).Replace("　", " ")
+                Dim d74 As String = Util.checkDBNullValue(row("D74")).Replace("　", " ")
+                Dim d72Index As Integer = d72.IndexOf("要")
+                Dim d73Index As Integer = d73.IndexOf("要")
+                Dim d74Index As Integer = d74.IndexOf("要")
+                If d72Index >= 0 Then
+                    Dim d72you As String = d72.Substring(d72Index, d72.Length - d72Index)
+                    decisionList.Add(d72you.Split(" ")(0))
+                End If
+                If d73Index >= 0 Then
+                    Dim d73you As String = d73.Substring(d73Index, d73.Length - d73Index)
+                    decisionList.Add(d73you.Split(" ")(0))
+                End If
+                If d74Index >= 0 Then
+                    Dim d74you As String = d74.Substring(d74Index, d74.Length - d74Index)
+                    decisionList.Add(d74you.Split(" ")(0))
+                End If
+            Else
+                'A4のデータの"要"が含まれてる文字取得
+                Dim d76 As String = Util.checkDBNullValue(row("D76")).Replace("　", " ")
+                Dim d77 As String = Util.checkDBNullValue(row("D77")).Replace("　", " ")
+                Dim d78 As String = Util.checkDBNullValue(row("D78")).Replace("　", " ")
+                Dim d79 As String = Util.checkDBNullValue(row("D79")).Replace("　", " ")
+                Dim d80 As String = Util.checkDBNullValue(row("D80")).Replace("　", " ")
+                Dim d76Index As Integer = d76.IndexOf("要")
+                Dim d77Index As Integer = d77.IndexOf("要")
+                Dim d78Index As Integer = d78.IndexOf("要")
+                Dim d79Index As Integer = d79.IndexOf("要")
+                Dim d80Index As Integer = d80.IndexOf("要")
+                If d76Index >= 0 Then
+                    Dim d76you As String = d76.Substring(d76Index, d76.Length - d76Index)
+                    decisionList.Add(d76you.Split(" ")(0))
+                End If
+                If d77Index >= 0 Then
+                    Dim d77you As String = d77.Substring(d77Index, d77.Length - d77Index)
+                    decisionList.Add(d77you.Split(" ")(0))
+                End If
+                If d78Index >= 0 Then
+                    Dim d78you As String = d78.Substring(d78Index, d78.Length - d78Index)
+                    decisionList.Add(d78you.Split(" ")(0))
+                End If
+                If d79Index >= 0 Then
+                    Dim d79you As String = d79.Substring(d79Index, d79.Length - d79Index)
+                    decisionList.Add(d79you.Split(" ")(0))
+                End If
+                If d80Index >= 0 Then
+                    Dim d80you As String = d80.Substring(d80Index, d80.Length - d80Index)
+                    decisionList.Add(d80you.Split(" ")(0))
+                End If
+            End If
+            Dim decision As String = ""
+            For i As Integer = 0 To decisionList.Count - 1
+                If i = 0 Then
+                    decision = decisionList(i)
+                Else
+                    decision = decision & " " & decisionList(i)
+                End If
+            Next
+            row("Decision") = decision
         Next
 
         dgvMaster.DataSource = dt
@@ -185,6 +256,18 @@ Public Class 受診者マスタ
 
         '幅設定等
         With dgvMaster
+            .Columns("MaxYmdB").Visible = False
+            .Columns("MaxYmdA").Visible = False
+
+            .Columns("D72").Visible = False
+            .Columns("D73").Visible = False
+            .Columns("D74").Visible = False
+            .Columns("D76").Visible = False
+            .Columns("D77").Visible = False
+            .Columns("D78").Visible = False
+            .Columns("D79").Visible = False
+            .Columns("D80").Visible = False
+
             With .Columns("List")
                 .DisplayIndex = 0
                 .HeaderText = "名簿"

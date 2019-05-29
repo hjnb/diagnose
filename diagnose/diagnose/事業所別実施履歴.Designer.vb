81a7df970b3c3d366f125626dbea81f6f25aa410
DESKTOP-OJIT7DO\yoshi <yoshitaka7144@gmail.com>
2019-05-29 17:04:20 +0900

    事業所別実施履歴：デザイン、封筒ボタン機能作成

--- diagnose/diagnose/事業所別実施履歴.Designer.vb
+++ diagnose/diagnose/事業所別実施履歴.Designer.vb
@@ -22,8 +22,190 @@ Partial Class 事業所別_実施履歴
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.indList = New System.Windows.Forms.ListBox()
+        Me.indLabel = New System.Windows.Forms.Label()
+        Me.dgvList = New System.Windows.Forms.DataGridView()
+        Me.btnPrint = New System.Windows.Forms.Button()
+        Me.btnPersonal = New System.Windows.Forms.Button()
+        Me.btnEnvelope = New System.Windows.Forms.Button()
+        Me.rbtnPreview = New System.Windows.Forms.RadioButton()
+        Me.rbtnPrint = New System.Windows.Forms.RadioButton()
+        Me.rbtnNaga4 = New System.Windows.Forms.RadioButton()
+        Me.rbtnNaga3 = New System.Windows.Forms.RadioButton()
+        Me.rbtnKaku2 = New System.Windows.Forms.RadioButton()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        CType(Me.dgvList, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'indList
+        '
+        Me.indList.BackColor = System.Drawing.SystemColors.Control
+        Me.indList.FormattingEnabled = True
+        Me.indList.ItemHeight = 12
+        Me.indList.Location = New System.Drawing.Point(25, 45)
+        Me.indList.Name = "indList"
+        Me.indList.Size = New System.Drawing.Size(188, 580)
+        Me.indList.TabIndex = 0
+        '
+        'indLabel
+        '
+        Me.indLabel.AutoSize = True
+        Me.indLabel.Font = New System.Drawing.Font("MS UI Gothic", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.indLabel.ForeColor = System.Drawing.Color.Blue
+        Me.indLabel.Location = New System.Drawing.Point(215, 17)
+        Me.indLabel.Name = "indLabel"
+        Me.indLabel.Size = New System.Drawing.Size(0, 15)
+        Me.indLabel.TabIndex = 1
+        '
+        'dgvList
+        '
+        Me.dgvList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvList.Location = New System.Drawing.Point(235, 66)
+        Me.dgvList.Name = "dgvList"
+        Me.dgvList.RowTemplate.Height = 21
+        Me.dgvList.Size = New System.Drawing.Size(769, 560)
+        Me.dgvList.TabIndex = 2
+        '
+        'btnPrint
+        '
+        Me.btnPrint.Location = New System.Drawing.Point(467, 9)
+        Me.btnPrint.Name = "btnPrint"
+        Me.btnPrint.Size = New System.Drawing.Size(75, 30)
+        Me.btnPrint.TabIndex = 3
+        Me.btnPrint.Text = "印刷"
+        Me.btnPrint.UseVisualStyleBackColor = True
+        '
+        'btnPersonal
+        '
+        Me.btnPersonal.Location = New System.Drawing.Point(541, 9)
+        Me.btnPersonal.Name = "btnPersonal"
+        Me.btnPersonal.Size = New System.Drawing.Size(75, 30)
+        Me.btnPersonal.TabIndex = 4
+        Me.btnPersonal.Text = "個人票"
+        Me.btnPersonal.UseVisualStyleBackColor = True
+        '
+        'btnEnvelope
+        '
+        Me.btnEnvelope.Location = New System.Drawing.Point(660, 9)
+        Me.btnEnvelope.Name = "btnEnvelope"
+        Me.btnEnvelope.Size = New System.Drawing.Size(75, 30)
+        Me.btnEnvelope.TabIndex = 5
+        Me.btnEnvelope.Text = "封筒"
+        Me.btnEnvelope.UseVisualStyleBackColor = True
+        '
+        'rbtnPreview
+        '
+        Me.rbtnPreview.AutoSize = True
+        Me.rbtnPreview.Location = New System.Drawing.Point(9, 6)
+        Me.rbtnPreview.Name = "rbtnPreview"
+        Me.rbtnPreview.Size = New System.Drawing.Size(63, 16)
+        Me.rbtnPreview.TabIndex = 6
+        Me.rbtnPreview.TabStop = True
+        Me.rbtnPreview.Text = "ﾌﾟﾚﾋﾞｭｰ"
+        Me.rbtnPreview.UseVisualStyleBackColor = True
+        '
+        'rbtnPrint
+        '
+        Me.rbtnPrint.AutoSize = True
+        Me.rbtnPrint.Location = New System.Drawing.Point(81, 6)
+        Me.rbtnPrint.Name = "rbtnPrint"
+        Me.rbtnPrint.Size = New System.Drawing.Size(47, 16)
+        Me.rbtnPrint.TabIndex = 7
+        Me.rbtnPrint.TabStop = True
+        Me.rbtnPrint.Text = "印刷"
+        Me.rbtnPrint.UseVisualStyleBackColor = True
+        '
+        'rbtnNaga4
+        '
+        Me.rbtnNaga4.AutoSize = True
+        Me.rbtnNaga4.Location = New System.Drawing.Point(3, 3)
+        Me.rbtnNaga4.Name = "rbtnNaga4"
+        Me.rbtnNaga4.Size = New System.Drawing.Size(65, 16)
+        Me.rbtnNaga4.TabIndex = 8
+        Me.rbtnNaga4.TabStop = True
+        Me.rbtnNaga4.Text = "長形4号"
+        Me.rbtnNaga4.UseVisualStyleBackColor = True
+        '
+        'rbtnNaga3
+        '
+        Me.rbtnNaga3.AutoSize = True
+        Me.rbtnNaga3.Checked = True
+        Me.rbtnNaga3.Location = New System.Drawing.Point(74, 3)
+        Me.rbtnNaga3.Name = "rbtnNaga3"
+        Me.rbtnNaga3.Size = New System.Drawing.Size(65, 16)
+        Me.rbtnNaga3.TabIndex = 9
+        Me.rbtnNaga3.TabStop = True
+        Me.rbtnNaga3.Text = "長形3号"
+        Me.rbtnNaga3.UseVisualStyleBackColor = True
+        '
+        'rbtnKaku2
+        '
+        Me.rbtnKaku2.AutoSize = True
+        Me.rbtnKaku2.Location = New System.Drawing.Point(159, 3)
+        Me.rbtnKaku2.Name = "rbtnKaku2"
+        Me.rbtnKaku2.Size = New System.Drawing.Size(65, 16)
+        Me.rbtnKaku2.TabIndex = 10
+        Me.rbtnKaku2.TabStop = True
+        Me.rbtnKaku2.Text = "角形2号"
+        Me.rbtnKaku2.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.rbtnPreview)
+        Me.Panel1.Controls.Add(Me.rbtnPrint)
+        Me.Panel1.Location = New System.Drawing.Point(738, 10)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(157, 26)
+        Me.Panel1.TabIndex = 11
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.rbtnNaga4)
+        Me.Panel2.Controls.Add(Me.rbtnNaga3)
+        Me.Panel2.Controls.Add(Me.rbtnKaku2)
+        Me.Panel2.Location = New System.Drawing.Point(660, 42)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(235, 22)
+        Me.Panel2.TabIndex = 12
+        '
+        '事業所別_実施履歴
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1025, 659)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.btnEnvelope)
+        Me.Controls.Add(Me.btnPersonal)
+        Me.Controls.Add(Me.btnPrint)
+        Me.Controls.Add(Me.dgvList)
+        Me.Controls.Add(Me.indLabel)
+        Me.Controls.Add(Me.indList)
+        Me.Name = "事業所別_実施履歴"
         Me.Text = "事業所別_実施履歴"
+        CType(Me.dgvList, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents indList As System.Windows.Forms.ListBox
+    Friend WithEvents indLabel As System.Windows.Forms.Label
+    Friend WithEvents dgvList As System.Windows.Forms.DataGridView
+    Friend WithEvents btnPrint As System.Windows.Forms.Button
+    Friend WithEvents btnPersonal As System.Windows.Forms.Button
+    Friend WithEvents btnEnvelope As System.Windows.Forms.Button
+    Friend WithEvents rbtnPreview As System.Windows.Forms.RadioButton
+    Friend WithEvents rbtnPrint As System.Windows.Forms.RadioButton
+    Friend WithEvents rbtnNaga4 As System.Windows.Forms.RadioButton
+    Friend WithEvents rbtnNaga3 As System.Windows.Forms.RadioButton
+    Friend WithEvents rbtnKaku2 As System.Windows.Forms.RadioButton
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
 End Class
--- diagnose/diagnose/事業所別実施履歴.vb
+++ diagnose/diagnose/事業所別実施履歴.vb
@@ -1,3 +1,478 @@
-﻿Public Class 事業所別_実施履歴
+﻿Imports System.Data.OleDb
+Imports Microsoft.Office.Interop
+Imports System.Runtime.InteropServices
+
+Public Class 事業所別_実施履歴
+    ''' <summary>
+    ''' 行ヘッダーのカレントセルを表す三角マークを非表示に設定する為のクラス。
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Class dgvRowHeaderCell
+
+        'DataGridViewRowHeaderCell を継承
+        Inherits DataGridViewRowHeaderCell
+
+        'DataGridViewHeaderCell.Paint をオーバーライドして行ヘッダーを描画
+        Protected Overrides Sub Paint(ByVal graphics As Graphics, ByVal clipBounds As Rectangle, _
+           ByVal cellBounds As Rectangle, ByVal rowIndex As Integer, ByVal cellState As DataGridViewElementStates, _
+           ByVal value As Object, ByVal formattedValue As Object, ByVal errorText As String, _
+           ByVal cellStyle As DataGridViewCellStyle, ByVal advancedBorderStyle As DataGridViewAdvancedBorderStyle, _
+           ByVal paintParts As DataGridViewPaintParts)
+            '標準セルの描画からセル内容の背景だけ除いた物を描画(-5)
+            MyBase.Paint(graphics, clipBounds, cellBounds, rowIndex, cellState, value, _
+                     formattedValue, errorText, cellStyle, advancedBorderStyle, _
+                     Not DataGridViewPaintParts.ContentBackground)
+        End Sub
+
+    End Class
+
+    ''' <summary>
+    ''' loadイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub 事業所別_実施履歴_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Me.WindowState = FormWindowState.Maximized
+
+        '印刷ラジオボタン初期値設定
+        initPrintState()
+
+        '事業所リスト初期設定
+        initIndList()
+
+        'データグリッドビュー初期設定
+        initDgvList()
+    End Sub
+
+    ''' <summary>
+    ''' 事業所リスト初期設定
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub initIndList()
+        Dim cn As New ADODB.Connection()
+        cn.Open(topForm.DB_Diagnose)
+        Dim sql As String = "select Ind from IndM order by Kana"
+        Dim rs As New ADODB.Recordset
+        rs.Open(sql, cn, ADODB.CursorTypeEnum.adOpenForwardOnly, ADODB.LockTypeEnum.adLockOptimistic)
+        While Not rs.EOF
+            Dim ind As String = Util.checkDBNullValue(rs.Fields("Ind").Value)
+            indList.Items.Add(ind)
+            rs.MoveNext()
+        End While
+        rs.Close()
+        cn.Close()
+    End Sub
+
+    ''' <summary>
+    ''' データグリッドビュー初期設定
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub initDgvList()
+        Util.EnableDoubleBuffering(dgvList)
+
+        With dgvList
+            .AllowUserToAddRows = False '行追加禁止
+            .AllowUserToResizeColumns = False '列の幅をユーザーが変更できないようにする
+            .AllowUserToResizeRows = False '行の高さをユーザーが変更できないようにする
+            .AllowUserToDeleteRows = False '行削除禁止
+            .BorderStyle = BorderStyle.FixedSingle
+            .MultiSelect = False
+            .SelectionMode = DataGridViewSelectionMode.CellSelect
+            .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft
+            .DefaultCellStyle.BackColor = Color.FromKnownColor(KnownColor.Control)
+            .DefaultCellStyle.ForeColor = Color.Black
+            .DefaultCellStyle.SelectionBackColor = Color.FromKnownColor(KnownColor.Control)
+            .DefaultCellStyle.SelectionForeColor = Color.Black
+            .RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing
+            .ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+            .ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+            .RowHeadersWidth = 30
+            .ColumnHeadersHeight = 18
+            .RowTemplate.Height = 18
+            .RowTemplate.HeaderCell = New dgvRowHeaderCell() '行ヘッダの三角マークを非表示に
+            .BackgroundColor = Color.FromKnownColor(KnownColor.Control)
+            .ShowCellToolTips = False
+            .EnableHeadersVisualStyles = False
+            .Font = New Font("ＭＳ Ｐゴシック", 9)
+            .ReadOnly = False
+        End With
+    End Sub
+
+    ''' <summary>
+    ''' 対象事業所のリスト表示
+    ''' </summary>
+    ''' <param name="ind">事業所名</param>
+    ''' <remarks></remarks>
+    Private Sub displayDgvList(ind As String)
+        '内容クリア
+        dgvList.Columns.Clear()
+
+        'データ取得
+        Dim cnn As New ADODB.Connection
+        cnn.Open(topForm.DB_Diagnose)
+        Dim rs As New ADODB.Recordset
+        Dim sql As String = "select Nam, Birth, Int((Format(NOW(),'YYYYMMDD')-Format(Birth, 'YYYYMMDD'))/10000) as Age from UsrM where Ind = '" & ind & "' order by Kana"
+        rs.Open(sql, cnn, ADODB.CursorTypeEnum.adOpenForwardOnly, ADODB.LockTypeEnum.adLockReadOnly)
+        Dim da As OleDbDataAdapter = New OleDbDataAdapter()
+        Dim ds As DataSet = New DataSet()
+        da.Fill(ds, rs, "UsrM")
+        Dim dt As DataTable = ds.Tables("UsrM")
+
+        '列追加
+        dt.Columns.Add("Check", GetType(Boolean)) 'チェックボックス
+        dt.Columns.Add("J1", GetType(String)) '実施日1
+        dt.Columns.Add("J2", GetType(String)) '実施日2
+        dt.Columns.Add("J3", GetType(String)) '実施日3
+        dt.Columns.Add("J4", GetType(String)) '実施日4
+        dt.Columns.Add("J5", GetType(String)) '実施日5
+        For Each row As DataRow In dt.Rows
+            row("Check") = False
+        Next
+
+        '表示
+        dgvList.DataSource = dt
+
+        '幅設定等
+        With dgvList
+
+            'サイズ
+            If dgvList.Rows.Count <= 30 Then
+                .Size = New Size(752, 560)
+            Else
+                .Size = New Size(769, 560)
+            End If
+
+            With .Columns("Check")
+                .DisplayIndex = 0
+                .HeaderText = ""
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+                .Width = 30
+            End With
+            With .Columns("Nam")
+                .HeaderText = "氏名"
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+                .Width = 95
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft
+                .ReadOnly = True
+            End With
+            With .Columns("Birth")
+                .HeaderText = "生年月日"
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+                .Width = 75
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+                .ReadOnly = True
+            End With
+            With .Columns("Age")
+                .HeaderText = "年齢"
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+                .Width = 45
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+                .ReadOnly = True
+            End With
+            For i As Integer = 1 To 5
+                With .Columns("J" & i)
+                    .HeaderText = i
+                    .SortMode = DataGridViewColumnSortMode.NotSortable
+                    .Width = 95
+                    .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+                    .ReadOnly = True
+                End With
+            Next
+        End With
+    End Sub
+
+    ''' <summary>
+    ''' CellFormatting
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub dgvList_CellFormatting(sender As Object, e As System.Windows.Forms.DataGridViewCellFormattingEventArgs) Handles dgvList.CellFormatting
+        If e.RowIndex >= 0 AndAlso e.ColumnIndex = 1 Then
+            '生年月日を和暦に
+            e.Value = Util.convADStrToWarekiStr(e.Value)
+            e.FormattingApplied = True
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' CellPaintingイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub dgvList_CellPainting(sender As Object, e As System.Windows.Forms.DataGridViewCellPaintingEventArgs) Handles dgvList.CellPainting
+        '行ヘッダーかどうか調べる
+        If e.ColumnIndex < 0 AndAlso e.RowIndex >= 0 Then
+            'セルを描画する
+            e.Paint(e.ClipBounds, DataGridViewPaintParts.All)
+
+            '行番号を描画する範囲を決定する
+            'e.AdvancedBorderStyleやe.CellStyle.Paddingは無視しています
+            Dim indexRect As Rectangle = e.CellBounds
+            indexRect.Inflate(-2, -2)
+            '行番号を描画する
+            TextRenderer.DrawText(e.Graphics, _
+                (e.RowIndex + 1).ToString(), _
+                e.CellStyle.Font, _
+                indexRect, _
+                e.CellStyle.ForeColor, _
+                TextFormatFlags.HorizontalCenter Or TextFormatFlags.VerticalCenter)
+            '描画が完了したことを知らせる
+            e.Handled = True
+        End If
+        '選択したセルに枠を付ける
+        If e.ColumnIndex >= 0 AndAlso e.RowIndex >= 0 AndAlso (e.PaintParts And DataGridViewPaintParts.Background) = DataGridViewPaintParts.Background Then
+            e.Graphics.FillRectangle(New SolidBrush(e.CellStyle.BackColor), e.CellBounds)
+
+            If (e.PaintParts And DataGridViewPaintParts.SelectionBackground) = DataGridViewPaintParts.SelectionBackground AndAlso (e.State And DataGridViewElementStates.Selected) = DataGridViewElementStates.Selected Then
+                e.Graphics.DrawRectangle(New Pen(Color.Black, 2I), e.CellBounds.X + 1I, e.CellBounds.Y + 1I, e.CellBounds.Width - 3I, e.CellBounds.Height - 3I)
+            End If
+
+            Dim pParts As DataGridViewPaintParts = e.PaintParts And Not DataGridViewPaintParts.Background
+            e.Paint(e.ClipBounds, pParts)
+            e.Handled = True
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' 印刷ラジオボタン初期値設定
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub initPrintState()
+        Dim state As String = Util.getIniString("System", "Printer", topForm.iniFilePath)
+        If state = "Y" Then
+            rbtnPrint.Checked = True
+        Else
+            rbtnPreview.Checked = True
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' ﾌﾟﾚﾋﾞｭｰラジオボタン値変更イベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub rbtnPreview_CheckedChanged(sender As Object, e As System.EventArgs) Handles rbtnPreview.CheckedChanged
+        If rbtnPreview.Checked = True Then
+            Util.putIniString("System", "Printer", "N", topForm.iniFilePath)
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' 印刷ラジオボタン値変更イベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub rbtnPrint_CheckedChanged(sender As Object, e As System.EventArgs) Handles rbtnPrint.CheckedChanged
+        If rbtnPrint.Checked = True Then
+            Util.putIniString("System", "Printer", "Y", topForm.iniFilePath)
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' 事業所リスト値変更イベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub indList_SelectedValueChanged(sender As Object, e As System.EventArgs) Handles indList.SelectedValueChanged
+        Dim ind As String = indList.Text
+        indLabel.Text = ind
+        displayDgvList(ind)
+    End Sub
+
+    ''' <summary>
+    ''' 封筒ボタンクリックイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub btnEnvelope_Click(sender As System.Object, e As System.EventArgs) Handles btnEnvelope.Click
+        '印刷データ作成
+        Dim namList As New List(Of String)
+        For Each row As DataGridViewRow In dgvList.Rows
+            If row.Cells("Check").Value Then
+                namList.Add(row.Cells("Nam").Value & "　様")
+            End If
+        Next
+
+        If rbtnNaga3.Checked Then
+            printNaga3(namList)
+        ElseIf rbtnNaga4.Checked Then
+            printNaga4(namList)
+        ElseIf rbtnKaku2.Checked Then
+            printKaku2(namList)
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' 長形3号印刷
+    ''' </summary>
+    ''' <param name="namList"></param>
+    ''' <remarks></remarks>
+    Private Sub printNaga3(namList As List(Of String))
+        'エクセル
+        Dim objExcel As Excel.Application = CreateObject("Excel.Application")
+        Dim objWorkBooks As Excel.Workbooks = objExcel.Workbooks
+        Dim objWorkBook As Excel.Workbook = objWorkBooks.Open(topForm.excelFilePass)
+        Dim oSheet As Excel.Worksheet = objWorkBook.Worksheets("長形３号")
+        objExcel.Calculation = Excel.XlCalculation.xlCalculationManual
+        objExcel.ScreenUpdating = False
+
+        '削除
+        oSheet.Range("J2").Value = ""
+        oSheet.Range("E8").Value = ""
+        oSheet.Range("E9").Value = ""
+        oSheet.Range("H21").Value = ""
+        oSheet.Range("J21").Value = ""
+
+        '必要枚数コピペ
+        For i As Integer = 0 To namList.Count - 2
+            Dim xlPasteRange As Excel.Range = oSheet.Range("A" & (44 + (43 * i))) 'ペースト先
+            oSheet.Rows("1:43").copy(xlPasteRange)
+            oSheet.HPageBreaks.Add(oSheet.Range("A" & (44 + (43 * i)))) '改ページ
+        Next
+
+        'データ書き込み
+        For i As Integer = 0 To namList.Count - 1
+            oSheet.Range("E" & (11 + 43 * i)).Value = namList(i)
+        Next
+
+        objExcel.Calculation = Excel.XlCalculation.xlCalculationAutomatic
+        objExcel.ScreenUpdating = True
+
+        '変更保存確認ダイアログ非表示
+        objExcel.DisplayAlerts = False
+
+        '印刷
+        If rbtnPrint.Checked = True Then
+            oSheet.PrintOut()
+        ElseIf rbtnPreview.Checked = True Then
+            objExcel.Visible = True
+            oSheet.PrintPreview(1)
+        End If
+
+        ' EXCEL解放
+        objExcel.Quit()
+        Marshal.ReleaseComObject(objWorkBook)
+        Marshal.ReleaseComObject(objExcel)
+        oSheet = Nothing
+        objWorkBook = Nothing
+        objExcel = Nothing
+    End Sub
+
+    ''' <summary>
+    ''' 長形4号印刷
+    ''' </summary>
+    ''' <param name="namList"></param>
+    ''' <remarks></remarks>
+    Private Sub printNaga4(namList As List(Of String))
+        'エクセル
+        Dim objExcel As Excel.Application = CreateObject("Excel.Application")
+        Dim objWorkBooks As Excel.Workbooks = objExcel.Workbooks
+        Dim objWorkBook As Excel.Workbook = objWorkBooks.Open(topForm.excelFilePass)
+        Dim oSheet As Excel.Worksheet = objWorkBook.Worksheets("長形４号")
+        objExcel.Calculation = Excel.XlCalculation.xlCalculationManual
+        objExcel.ScreenUpdating = False
+
+        '削除
+        oSheet.Range("F2").Value = ""
+        oSheet.Range("C7").Value = ""
+        oSheet.Range("C8").Value = ""
+        oSheet.Range("D19").Value = ""
+        oSheet.Range("G19").Value = ""
+
+        '必要枚数コピペ
+        For i As Integer = 0 To namList.Count - 2
+            Dim xlPasteRange As Excel.Range = oSheet.Range("A" & (29 + (28 * i))) 'ペースト先
+            oSheet.Rows("1:28").copy(xlPasteRange)
+            oSheet.HPageBreaks.Add(oSheet.Range("A" & (29 + (28 * i)))) '改ページ
+        Next
+
+        'データ書き込み
+        For i As Integer = 0 To namList.Count - 1
+            oSheet.Range("C" & (10 + 28 * i)).Value = namList(i)
+        Next
+
+        objExcel.Calculation = Excel.XlCalculation.xlCalculationAutomatic
+        objExcel.ScreenUpdating = True
+
+        '変更保存確認ダイアログ非表示
+        objExcel.DisplayAlerts = False
+
+        '印刷
+        If rbtnPrint.Checked = True Then
+            oSheet.PrintOut()
+        ElseIf rbtnPreview.Checked = True Then
+            objExcel.Visible = True
+            oSheet.PrintPreview(1)
+        End If
+
+        ' EXCEL解放
+        objExcel.Quit()
+        Marshal.ReleaseComObject(objWorkBook)
+        Marshal.ReleaseComObject(objExcel)
+        oSheet = Nothing
+        objWorkBook = Nothing
+        objExcel = Nothing
+    End Sub
+
+    ''' <summary>
+    ''' 角形2号印刷
+    ''' </summary>
+    ''' <param name="namList"></param>
+    ''' <remarks></remarks>
+    Private Sub printKaku2(namList As List(Of String))
+        'エクセル
+        Dim objExcel As Excel.Application = CreateObject("Excel.Application")
+        Dim objWorkBooks As Excel.Workbooks = objExcel.Workbooks
+        Dim objWorkBook As Excel.Workbook = objWorkBooks.Open(topForm.excelFilePass)
+        Dim oSheet As Excel.Worksheet = objWorkBook.Worksheets("角形２号")
+        objExcel.Calculation = Excel.XlCalculation.xlCalculationManual
+        objExcel.ScreenUpdating = False
+
+        '削除
+        oSheet.Range("Y6").Value = ""
+        oSheet.Range("K19").Value = ""
+        oSheet.Range("K20").Value = ""
+
+        '必要枚数コピペ
+        For i As Integer = 0 To namList.Count - 2
+            Dim xlPasteRange As Excel.Range = oSheet.Range("A" & (58 + (57 * i))) 'ペースト先
+            oSheet.Rows("1:57").copy(xlPasteRange)
+            oSheet.HPageBreaks.Add(oSheet.Range("A" & (58 + (57 * i)))) '改ページ
+        Next
+
+        'データ書き込み
+        For i As Integer = 0 To namList.Count - 1
+            oSheet.Range("K" & (22 + 57 * i)).Value = namList(i)
+        Next
+
+        objExcel.Calculation = Excel.XlCalculation.xlCalculationAutomatic
+        objExcel.ScreenUpdating = True
+
+        '変更保存確認ダイアログ非表示
+        objExcel.DisplayAlerts = False
+
+        '印刷
+        If rbtnPrint.Checked = True Then
+            oSheet.PrintOut()
+        ElseIf rbtnPreview.Checked = True Then
+            objExcel.Visible = True
+            oSheet.PrintPreview(1)
+        End If
+
+        ' EXCEL解放
+        objExcel.Quit()
+        Marshal.ReleaseComObject(objWorkBook)
+        Marshal.ReleaseComObject(objExcel)
+        oSheet = Nothing
+        objWorkBook = Nothing
+        objExcel = Nothing
+    End Sub
 
 End Class

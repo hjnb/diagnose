1febac00c0433baabf9d3d0314058e9c34f356cd
DESKTOP-OJIT7DO\yoshi <yoshitaka7144@gmail.com>
2019-07-04 16:38:40 +0900

    健診結果報告書：デザイン作成等。

--- diagnose/diagnose/健診結果報告書.Designer.vb
+++ diagnose/diagnose/健診結果報告書.Designer.vb
@@ -22,8 +22,216 @@ Partial Class 健診結果報告書
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.indBox = New System.Windows.Forms.ComboBox()
+        Me.fromYmdBox = New ymdBox.ymdBox()
+        Me.toYmdBox = New ymdBox.ymdBox()
+        Me.btnExecute = New System.Windows.Forms.Button()
+        Me.btnPrint = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.totalLabel = New System.Windows.Forms.Label()
+        Me.syokenLabel = New System.Windows.Forms.Label()
+        Me.sijiLabel = New System.Windows.Forms.Label()
+        Me.dgvResult = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvResult, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label1.Location = New System.Drawing.Point(56, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "事業所名"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label2.Location = New System.Drawing.Point(56, 71)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 16)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "受診日"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label3.Location = New System.Drawing.Point(56, 488)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 16)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "所見者数"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label4.Location = New System.Drawing.Point(56, 451)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(72, 16)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "受診者数"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label5.Location = New System.Drawing.Point(56, 525)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(88, 16)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "医師指示数"
+        '
+        'indBox
+        '
+        Me.indBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.indBox.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.indBox.FormattingEnabled = True
+        Me.indBox.Location = New System.Drawing.Point(158, 24)
+        Me.indBox.Name = "indBox"
+        Me.indBox.Size = New System.Drawing.Size(317, 24)
+        Me.indBox.TabIndex = 5
+        '
+        'fromYmdBox
+        '
+        Me.fromYmdBox.boxType = 3
+        Me.fromYmdBox.DateText = ""
+        Me.fromYmdBox.EraLabelText = "R01"
+        Me.fromYmdBox.EraText = ""
+        Me.fromYmdBox.Location = New System.Drawing.Point(157, 55)
+        Me.fromYmdBox.MonthLabelText = "07"
+        Me.fromYmdBox.MonthText = ""
+        Me.fromYmdBox.Name = "fromYmdBox"
+        Me.fromYmdBox.Size = New System.Drawing.Size(145, 46)
+        Me.fromYmdBox.TabIndex = 6
+        Me.fromYmdBox.textReadOnly = False
+        '
+        'toYmdBox
+        '
+        Me.toYmdBox.boxType = 3
+        Me.toYmdBox.DateText = ""
+        Me.toYmdBox.EraLabelText = "R01"
+        Me.toYmdBox.EraText = ""
+        Me.toYmdBox.Location = New System.Drawing.Point(334, 55)
+        Me.toYmdBox.MonthLabelText = "07"
+        Me.toYmdBox.MonthText = ""
+        Me.toYmdBox.Name = "toYmdBox"
+        Me.toYmdBox.Size = New System.Drawing.Size(145, 46)
+        Me.toYmdBox.TabIndex = 7
+        Me.toYmdBox.textReadOnly = False
+        '
+        'btnExecute
+        '
+        Me.btnExecute.Location = New System.Drawing.Point(328, 131)
+        Me.btnExecute.Name = "btnExecute"
+        Me.btnExecute.Size = New System.Drawing.Size(69, 31)
+        Me.btnExecute.TabIndex = 8
+        Me.btnExecute.Text = "実行"
+        Me.btnExecute.UseVisualStyleBackColor = True
+        '
+        'btnPrint
+        '
+        Me.btnPrint.Location = New System.Drawing.Point(396, 131)
+        Me.btnPrint.Name = "btnPrint"
+        Me.btnPrint.Size = New System.Drawing.Size(69, 31)
+        Me.btnPrint.TabIndex = 9
+        Me.btnPrint.Text = "印刷"
+        Me.btnPrint.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label6.Location = New System.Drawing.Point(306, 71)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(24, 16)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "～"
+        '
+        'totalLabel
+        '
+        Me.totalLabel.AutoSize = True
+        Me.totalLabel.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.totalLabel.Location = New System.Drawing.Point(159, 448)
+        Me.totalLabel.Name = "totalLabel"
+        Me.totalLabel.Size = New System.Drawing.Size(0, 16)
+        Me.totalLabel.TabIndex = 11
+        '
+        'syokenLabel
+        '
+        Me.syokenLabel.AutoSize = True
+        Me.syokenLabel.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.syokenLabel.Location = New System.Drawing.Point(159, 485)
+        Me.syokenLabel.Name = "syokenLabel"
+        Me.syokenLabel.Size = New System.Drawing.Size(0, 16)
+        Me.syokenLabel.TabIndex = 12
+        '
+        'sijiLabel
+        '
+        Me.sijiLabel.AutoSize = True
+        Me.sijiLabel.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.sijiLabel.Location = New System.Drawing.Point(159, 522)
+        Me.sijiLabel.Name = "sijiLabel"
+        Me.sijiLabel.Size = New System.Drawing.Size(0, 16)
+        Me.sijiLabel.TabIndex = 13
+        '
+        'dgvResult
+        '
+        Me.dgvResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvResult.Location = New System.Drawing.Point(59, 188)
+        Me.dgvResult.Name = "dgvResult"
+        Me.dgvResult.RowTemplate.Height = 21
+        Me.dgvResult.Size = New System.Drawing.Size(337, 242)
+        Me.dgvResult.TabIndex = 14
+        '
+        '健診結果報告書
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(735, 615)
+        Me.Controls.Add(Me.dgvResult)
+        Me.Controls.Add(Me.sijiLabel)
+        Me.Controls.Add(Me.syokenLabel)
+        Me.Controls.Add(Me.totalLabel)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.btnPrint)
+        Me.Controls.Add(Me.btnExecute)
+        Me.Controls.Add(Me.toYmdBox)
+        Me.Controls.Add(Me.fromYmdBox)
+        Me.Controls.Add(Me.indBox)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "健診結果報告書"
         Me.Text = "健診結果報告書"
+        CType(Me.dgvResult, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents indBox As System.Windows.Forms.ComboBox
+    Friend WithEvents fromYmdBox As ymdBox.ymdBox
+    Friend WithEvents toYmdBox As ymdBox.ymdBox
+    Friend WithEvents btnExecute As System.Windows.Forms.Button
+    Friend WithEvents btnPrint As System.Windows.Forms.Button
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents totalLabel As System.Windows.Forms.Label
+    Friend WithEvents syokenLabel As System.Windows.Forms.Label
+    Friend WithEvents sijiLabel As System.Windows.Forms.Label
+    Friend WithEvents dgvResult As System.Windows.Forms.DataGridView
 End Class
--- diagnose/diagnose/健診結果報告書.vb
+++ diagnose/diagnose/健診結果報告書.vb
@@ -1,3 +1,231 @@
 ﻿Public Class 健診結果報告書
 
+    '表示用データテーブル
+    Private dtResult As DataTable = New DataTable()
+
+    ''' <summary>
+    ''' 行ヘッダーのカレントセルを表す三角マークを非表示に設定する為のクラス。
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Class dgvRowHeaderCell
+
+        'DataGridViewRowHeaderCell を継承
+        Inherits DataGridViewRowHeaderCell
+
+        'DataGridViewHeaderCell.Paint をオーバーライドして行ヘッダーを描画
+        Protected Overrides Sub Paint(ByVal graphics As Graphics, ByVal clipBounds As Rectangle, _
+           ByVal cellBounds As Rectangle, ByVal rowIndex As Integer, ByVal cellState As DataGridViewElementStates, _
+           ByVal value As Object, ByVal formattedValue As Object, ByVal errorText As String, _
+           ByVal cellStyle As DataGridViewCellStyle, ByVal advancedBorderStyle As DataGridViewAdvancedBorderStyle, _
+           ByVal paintParts As DataGridViewPaintParts)
+            '標準セルの描画からセル内容の背景だけ除いた物を描画(-5)
+            MyBase.Paint(graphics, clipBounds, cellBounds, rowIndex, cellState, value, _
+                     formattedValue, errorText, cellStyle, advancedBorderStyle, _
+                     Not DataGridViewPaintParts.ContentBackground)
+        End Sub
+
+    End Class
+
+    ''' <summary>
+    ''' loadイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub 健診結果報告書_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Me.WindowState = FormWindowState.Maximized
+        Me.KeyPreview = True
+
+        '事業所名ボックス初期設定
+        initIndBox()
+
+        '日付ボックスに現在日付を設定
+        Dim nowStr As String = DateTime.Now.ToString("yyyy/MM/dd")
+        fromYmdBox.setADStr(nowStr)
+        toYmdBox.setADStr(nowStr)
+
+        'データグリッドビュー初期設定
+        initDgvResult()
+    End Sub
+
+    ''' <summary>
+    ''' keyDownイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub 健診結果報告書_KeyDown(sender As Object, e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            If e.Control = False Then
+                Me.SelectNextControl(Me.ActiveControl, Not e.Shift, True, True, True)
+            End If
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' 事業所名ボックス初期設定
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub initIndBox()
+        indBox.ImeMode = Windows.Forms.ImeMode.Hiragana
+        indBox.Items.Clear()
+        Dim cn As New ADODB.Connection()
+        cn.Open(topForm.DB_Diagnose)
+        Dim sql As String = "SELECT Ind FROM IndM ORDER BY Kana"
+        Dim rs As New ADODB.Recordset
+        rs.Open(sql, cn, ADODB.CursorTypeEnum.adOpenForwardOnly, ADODB.LockTypeEnum.adLockOptimistic)
+        While Not rs.EOF
+            Dim txt As String = Util.checkDBNullValue(rs.Fields("Ind").Value)
+            indBox.Items.Add(txt)
+            rs.MoveNext()
+        End While
+        rs.Close()
+        cn.Close()
+    End Sub
+
+    ''' <summary>
+    ''' データグリッドビュー初期設定
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub initDgvResult()
+        Util.EnableDoubleBuffering(dgvResult)
+
+        With dgvResult
+            .AllowUserToAddRows = False '行追加禁止
+            .AllowUserToResizeColumns = False '列の幅をユーザーが変更できないようにする
+            .AllowUserToResizeRows = False '行の高さをユーザーが変更できないようにする
+            .AllowUserToDeleteRows = False '行削除禁止
+            .BorderStyle = BorderStyle.FixedSingle
+            .MultiSelect = False
+            .SelectionMode = DataGridViewSelectionMode.CellSelect
+            .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft
+            .DefaultCellStyle.BackColor = Color.FromKnownColor(KnownColor.Control)
+            .DefaultCellStyle.ForeColor = Color.Black
+            .DefaultCellStyle.SelectionBackColor = Color.FromKnownColor(KnownColor.Control)
+            .DefaultCellStyle.SelectionForeColor = Color.Black
+            .RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing
+            .ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+            .ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+            .ColumnHeadersHeight = 20
+            .RowHeadersWidth = 30
+            .RowTemplate.Height = 20
+            .RowTemplate.HeaderCell = New dgvRowHeaderCell() '行ヘッダの三角マークを非表示に
+            .BackgroundColor = Color.FromKnownColor(KnownColor.Control)
+            .ShowCellToolTips = False
+            .EnableHeadersVisualStyles = False
+            .Font = New Font("ＭＳ Ｐゴシック", 10)
+            .ReadOnly = True
+        End With
+
+        '列追加、空の行追加
+        Dim itemArray() As String = {"聴力　1000Hz", "　　　　4000Hz", "胸部Ｘ線", "血圧", "貧血", "肝機能", "血中脂質", "血糖", "尿　糖", "尿　蛋白", "心電図"}
+        dtResult.Columns.Add("Item", GetType(String))
+        dtResult.Columns.Add("JNum", GetType(String))
+        dtResult.Columns.Add("SNum", GetType(String))
+        For i = 0 To 10
+            Dim row As DataRow = dtResult.NewRow()
+            row(0) = itemArray(i)
+            row(1) = ""
+            row(2) = ""
+            dtResult.Rows.Add(row)
+        Next
+
+        '表示
+        dgvResult.DataSource = dtResult
+
+        '幅設定等
+        With dgvResult
+            With .Columns("Item")
+                .HeaderText = "検査項目"
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft
+                .Width = 115
+            End With
+            With .Columns("JNum")
+                .HeaderText = "実施者数"
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+                .Width = 95
+            End With
+            With .Columns("SNum")
+                .HeaderText = "所見者数"
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+                .Width = 95
+            End With
+        End With
+
+    End Sub
+
+    ''' <summary>
+    ''' CellPaintingイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub dgvMaster_CellPainting(sender As Object, e As System.Windows.Forms.DataGridViewCellPaintingEventArgs) Handles dgvResult.CellPainting
+        '行ヘッダーかどうか調べる
+        If e.ColumnIndex < 0 AndAlso e.RowIndex >= 0 Then
+            'セルを描画する
+            e.Paint(e.ClipBounds, DataGridViewPaintParts.All)
+
+            '行番号を描画する範囲を決定する
+            'e.AdvancedBorderStyleやe.CellStyle.Paddingは無視しています
+            Dim indexRect As Rectangle = e.CellBounds
+            indexRect.Inflate(-2, -2)
+            '行番号を描画する
+            TextRenderer.DrawText(e.Graphics, _
+                (e.RowIndex + 1).ToString(), _
+                e.CellStyle.Font, _
+                indexRect, _
+                e.CellStyle.ForeColor, _
+                TextFormatFlags.HorizontalCenter Or TextFormatFlags.VerticalCenter)
+            '描画が完了したことを知らせる
+            e.Handled = True
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' 結果人数表示
+    ''' </summary>
+    ''' <param name="ind">事業所名</param>
+    ''' <param name="fromYmd">from日付</param>
+    ''' <param name="toYmd">to日付</param>
+    ''' <remarks></remarks>
+    Private Sub displayDgvResult(ind As String, fromYmd As String, toYmd As String)
+        '内容クリア
+        For Each row As DataRow In dtResult.Rows
+            '実施者
+            row("JNum") = ""
+            '所見者数
+            row("SNum") = ""
+        Next
+
+        'データ取得
+
+    End Sub
+
+    ''' <summary>
+    ''' 実行ボタンクリックイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub btnExecute_Click(sender As System.Object, e As System.EventArgs) Handles btnExecute.Click
+        '事業所名
+        Dim ind As String = indBox.Text
+        If ind = "" Then
+            MsgBox("事業所名を選択して下さい。", MsgBoxStyle.Exclamation)
+            indBox.DroppedDown = True
+            Return
+        End If
+
+        'from日付
+        Dim fromYmd As String = fromYmdBox.getADStr()
+        'to日付
+        Dim toYmd As String = toYmdBox.getADStr()
+
+        'データ表示
+        displayDgvResult(ind, fromYmd, toYmd)
+    End Sub
 End Class

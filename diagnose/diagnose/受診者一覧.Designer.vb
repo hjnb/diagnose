6481cec67676d0d528be59bc62c025fe676ce2d1
DESKTOP-OJIT7DO\yoshi <yoshitaka7144@gmail.com>
2019-07-18 14:48:39 +0900

    受診者一覧：作成

--- diagnose/diagnose/受診者一覧.Designer.vb
+++ diagnose/diagnose/受診者一覧.Designer.vb
@@ -22,8 +22,165 @@ Partial Class 受診者一覧
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnSearchWA = New System.Windows.Forms.Button()
+        Me.btnSearchRA = New System.Windows.Forms.Button()
+        Me.btnSearchYA = New System.Windows.Forms.Button()
+        Me.btnSearchMA = New System.Windows.Forms.Button()
+        Me.btnSearchHA = New System.Windows.Forms.Button()
+        Me.btnSearchNA = New System.Windows.Forms.Button()
+        Me.btnSearchTA = New System.Windows.Forms.Button()
+        Me.btnSearchSA = New System.Windows.Forms.Button()
+        Me.btnSearchKA = New System.Windows.Forms.Button()
+        Me.btnSearchA = New System.Windows.Forms.Button()
+        Me.dgvList = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvList, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MS UI Gothic", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Blue
+        Me.Label1.Location = New System.Drawing.Point(180, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(189, 15)
+        Me.Label1.TabIndex = 598
+        Me.Label1.Text = "ﾀﾞﾌﾞﾙｸﾘｯｸした項目で並べます"
+        '
+        'btnSearchWA
+        '
+        Me.btnSearchWA.Location = New System.Drawing.Point(82, 262)
+        Me.btnSearchWA.Name = "btnSearchWA"
+        Me.btnSearchWA.Size = New System.Drawing.Size(22, 19)
+        Me.btnSearchWA.TabIndex = 597
+        Me.btnSearchWA.Text = "ワ"
+        Me.btnSearchWA.UseVisualStyleBackColor = True
+        '
+        'btnSearchRA
+        '
+        Me.btnSearchRA.Location = New System.Drawing.Point(82, 244)
+        Me.btnSearchRA.Name = "btnSearchRA"
+        Me.btnSearchRA.Size = New System.Drawing.Size(22, 19)
+        Me.btnSearchRA.TabIndex = 596
+        Me.btnSearchRA.Text = "ラ"
+        Me.btnSearchRA.UseVisualStyleBackColor = True
+        '
+        'btnSearchYA
+        '
+        Me.btnSearchYA.Location = New System.Drawing.Point(82, 226)
+        Me.btnSearchYA.Name = "btnSearchYA"
+        Me.btnSearchYA.Size = New System.Drawing.Size(22, 19)
+        Me.btnSearchYA.TabIndex = 595
+        Me.btnSearchYA.Text = "ヤ"
+        Me.btnSearchYA.UseVisualStyleBackColor = True
+        '
+        'btnSearchMA
+        '
+        Me.btnSearchMA.Location = New System.Drawing.Point(82, 208)
+        Me.btnSearchMA.Name = "btnSearchMA"
+        Me.btnSearchMA.Size = New System.Drawing.Size(22, 19)
+        Me.btnSearchMA.TabIndex = 594
+        Me.btnSearchMA.Text = "マ"
+        Me.btnSearchMA.UseVisualStyleBackColor = True
+        '
+        'btnSearchHA
+        '
+        Me.btnSearchHA.Location = New System.Drawing.Point(82, 190)
+        Me.btnSearchHA.Name = "btnSearchHA"
+        Me.btnSearchHA.Size = New System.Drawing.Size(22, 19)
+        Me.btnSearchHA.TabIndex = 593
+        Me.btnSearchHA.Text = "ハ"
+        Me.btnSearchHA.UseVisualStyleBackColor = True
+        '
+        'btnSearchNA
+        '
+        Me.btnSearchNA.Location = New System.Drawing.Point(82, 172)
+        Me.btnSearchNA.Name = "btnSearchNA"
+        Me.btnSearchNA.Size = New System.Drawing.Size(22, 19)
+        Me.btnSearchNA.TabIndex = 592
+        Me.btnSearchNA.Text = "ナ"
+        Me.btnSearchNA.UseVisualStyleBackColor = True
+        '
+        'btnSearchTA
+        '
+        Me.btnSearchTA.Location = New System.Drawing.Point(82, 154)
+        Me.btnSearchTA.Name = "btnSearchTA"
+        Me.btnSearchTA.Size = New System.Drawing.Size(22, 19)
+        Me.btnSearchTA.TabIndex = 591
+        Me.btnSearchTA.Text = "タ"
+        Me.btnSearchTA.UseVisualStyleBackColor = True
+        '
+        'btnSearchSA
+        '
+        Me.btnSearchSA.Location = New System.Drawing.Point(82, 136)
+        Me.btnSearchSA.Name = "btnSearchSA"
+        Me.btnSearchSA.Size = New System.Drawing.Size(22, 19)
+        Me.btnSearchSA.TabIndex = 590
+        Me.btnSearchSA.Text = "サ"
+        Me.btnSearchSA.UseVisualStyleBackColor = True
+        '
+        'btnSearchKA
+        '
+        Me.btnSearchKA.Location = New System.Drawing.Point(82, 118)
+        Me.btnSearchKA.Name = "btnSearchKA"
+        Me.btnSearchKA.Size = New System.Drawing.Size(22, 19)
+        Me.btnSearchKA.TabIndex = 589
+        Me.btnSearchKA.Text = "カ"
+        Me.btnSearchKA.UseVisualStyleBackColor = True
+        '
+        'btnSearchA
+        '
+        Me.btnSearchA.Location = New System.Drawing.Point(82, 100)
+        Me.btnSearchA.Name = "btnSearchA"
+        Me.btnSearchA.Size = New System.Drawing.Size(22, 19)
+        Me.btnSearchA.TabIndex = 588
+        Me.btnSearchA.Text = "ア"
+        Me.btnSearchA.UseVisualStyleBackColor = True
+        '
+        'dgvList
+        '
+        Me.dgvList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvList.Location = New System.Drawing.Point(130, 77)
+        Me.dgvList.Name = "dgvList"
+        Me.dgvList.RowTemplate.Height = 21
+        Me.dgvList.Size = New System.Drawing.Size(739, 493)
+        Me.dgvList.TabIndex = 587
+        '
+        '受診者一覧
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(950, 616)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnSearchWA)
+        Me.Controls.Add(Me.btnSearchRA)
+        Me.Controls.Add(Me.btnSearchYA)
+        Me.Controls.Add(Me.btnSearchMA)
+        Me.Controls.Add(Me.btnSearchHA)
+        Me.Controls.Add(Me.btnSearchNA)
+        Me.Controls.Add(Me.btnSearchTA)
+        Me.Controls.Add(Me.btnSearchSA)
+        Me.Controls.Add(Me.btnSearchKA)
+        Me.Controls.Add(Me.btnSearchA)
+        Me.Controls.Add(Me.dgvList)
+        Me.Name = "受診者一覧"
         Me.Text = "受診者一覧"
+        CType(Me.dgvList, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnSearchWA As System.Windows.Forms.Button
+    Friend WithEvents btnSearchRA As System.Windows.Forms.Button
+    Friend WithEvents btnSearchYA As System.Windows.Forms.Button
+    Friend WithEvents btnSearchMA As System.Windows.Forms.Button
+    Friend WithEvents btnSearchHA As System.Windows.Forms.Button
+    Friend WithEvents btnSearchNA As System.Windows.Forms.Button
+    Friend WithEvents btnSearchTA As System.Windows.Forms.Button
+    Friend WithEvents btnSearchSA As System.Windows.Forms.Button
+    Friend WithEvents btnSearchKA As System.Windows.Forms.Button
+    Friend WithEvents btnSearchA As System.Windows.Forms.Button
+    Friend WithEvents dgvList As System.Windows.Forms.DataGridView
 End Class
--- diagnose/diagnose/受診者一覧.vb
+++ diagnose/diagnose/受診者一覧.vb
@@ -1,3 +1,230 @@
 ﻿Public Class 受診者一覧
 
+    ''' <summary>
+    ''' 行ヘッダーのカレントセルを表す三角マークを非表示に設定する為のクラス。
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Class dgvRowHeaderCell
+
+        'DataGridViewRowHeaderCell を継承
+        Inherits DataGridViewRowHeaderCell
+
+        'DataGridViewHeaderCell.Paint をオーバーライドして行ヘッダーを描画
+        Protected Overrides Sub Paint(ByVal graphics As Graphics, ByVal clipBounds As Rectangle, _
+           ByVal cellBounds As Rectangle, ByVal rowIndex As Integer, ByVal cellState As DataGridViewElementStates, _
+           ByVal value As Object, ByVal formattedValue As Object, ByVal errorText As String, _
+           ByVal cellStyle As DataGridViewCellStyle, ByVal advancedBorderStyle As DataGridViewAdvancedBorderStyle, _
+           ByVal paintParts As DataGridViewPaintParts)
+            '標準セルの描画からセル内容の背景だけ除いた物を描画(-5)
+            MyBase.Paint(graphics, clipBounds, cellBounds, rowIndex, cellState, value, _
+                     formattedValue, errorText, cellStyle, advancedBorderStyle, _
+                     Not DataGridViewPaintParts.ContentBackground)
+        End Sub
+
+    End Class
+
+    ''' <summary>
+    ''' loadイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub 受診者一覧_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Me.WindowState = FormWindowState.Maximized
+
+        'データグリッドビュー初期設定
+        initDgvList()
+
+        'データ表示
+        displayDgvList()
+    End Sub
+
+    ''' <summary>
+    ''' データグリッドビュー初期設定
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub initDgvList()
+        Util.EnableDoubleBuffering(dgvList)
+
+        With dgvList
+            .AllowUserToAddRows = False '行追加禁止
+            .AllowUserToResizeColumns = False '列の幅をユーザーが変更できないようにする
+            .AllowUserToResizeRows = False '行の高さをユーザーが変更できないようにする
+            .AllowUserToDeleteRows = False '行削除禁止
+            .BorderStyle = BorderStyle.FixedSingle
+            .MultiSelect = False
+            .SelectionMode = DataGridViewSelectionMode.FullRowSelect
+            .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft
+            .RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing
+            .ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+            .ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+            .RowHeadersWidth = 50
+            .RowTemplate.Height = 18
+            .RowTemplate.HeaderCell = New dgvRowHeaderCell() '行ヘッダの三角マークを非表示に
+            .BackgroundColor = Color.FromKnownColor(KnownColor.Control)
+            .ShowCellToolTips = False
+            .EnableHeadersVisualStyles = False
+            .Font = New Font("ＭＳ Ｐゴシック", 10)
+            .ReadOnly = True
+        End With
+    End Sub
+
+    ''' <summary>
+    ''' データ表示
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub displayDgvList()
+        '内容クリア
+        dgvList.Columns.Clear()
+
+        'データ取得
+        Dim dt As New DataTable()
+        dt.Columns.Add("Kana", GetType(String))
+        dt.Columns.Add("Nam", GetType(String))
+        dt.Columns.Add("Birth", GetType(String))
+        dt.Columns.Add("Age", GetType(Integer))
+        dt.Columns.Add("Ind", GetType(String))
+        Dim cnn As New ADODB.Connection
+        cnn.Open(topForm.DB_Diagnose)
+        Dim rs As New ADODB.Recordset
+        Dim sql As String = "select Kana, Nam, Birth, Int((Format(NOW(),'YYYYMMDD')-Format(Birth, 'YYYYMMDD'))/10000) as Age, Ind from UsrM order by Kana"
+        rs.Open(sql, cnn, ADODB.CursorTypeEnum.adOpenForwardOnly, ADODB.LockTypeEnum.adLockReadOnly)
+        While Not rs.EOF
+            Dim row As DataRow = dt.NewRow()
+            row("Kana") = Util.checkDBNullValue(rs.Fields("Kana").Value)
+            row("Nam") = Util.checkDBNullValue(rs.Fields("Nam").Value)
+            row("Birth") = Util.checkDBNullValue(rs.Fields("Birth").Value)
+            row("Age") = Util.checkDBNullValue(rs.Fields("Age").Value)
+            row("Ind") = Util.checkDBNullValue(rs.Fields("Ind").Value)
+            dt.Rows.Add(row)
+            rs.MoveNext()
+        End While
+        rs.Close()
+        cnn.Close()
+
+        '表示
+        dgvList.DataSource = dt
+        If Not IsNothing(dgvList.CurrentRow) Then
+            dgvList.CurrentRow.Selected = False
+        End If
+
+
+        '幅設定
+        With dgvList
+            With .Columns("Kana")
+                .HeaderText = "ｶﾅ"
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+                .Width = 120
+            End With
+            With .Columns("Nam")
+                .HeaderText = "氏名"
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+                .Width = 120
+            End With
+            With .Columns("Birth")
+                .HeaderText = "生年月日"
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+                .Width = 90
+            End With
+            With .Columns("Age")
+                .HeaderText = "年齢"
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+                .Width = 70
+            End With
+            With .Columns("Ind")
+                .HeaderText = "事業所"
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+                .Width = 270
+            End With
+        End With
+    End Sub
+
+    ''' <summary>
+    ''' CellFormattingイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub dgvList_CellFormatting(sender As Object, e As System.Windows.Forms.DataGridViewCellFormattingEventArgs) Handles dgvList.CellFormatting
+        If e.RowIndex >= 0 Then
+            Dim columnName As String = dgvList.Columns(e.ColumnIndex).Name
+            If columnName = "Birth" Then
+                e.Value = Util.convADStrToWarekiStr(Util.checkDBNullValue(dgvList("Birth", e.RowIndex).Value))
+                e.FormattingApplied = True
+            End If
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' CellPaintingイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub dgvList_CellPainting(sender As Object, e As System.Windows.Forms.DataGridViewCellPaintingEventArgs) Handles dgvList.CellPainting
+        '行ヘッダーかどうか調べる
+        If e.ColumnIndex < 0 AndAlso e.RowIndex >= 0 Then
+            'セルを描画する
+            e.Paint(e.ClipBounds, DataGridViewPaintParts.All)
+
+            '行番号を描画する範囲を決定する
+            'e.AdvancedBorderStyleやe.CellStyle.Paddingは無視しています
+            Dim indexRect As Rectangle = e.CellBounds
+            indexRect.Inflate(-2, -2)
+            '行番号を描画する
+            TextRenderer.DrawText(e.Graphics, _
+                (e.RowIndex + 1).ToString(), _
+                e.CellStyle.Font, _
+                indexRect, _
+                e.CellStyle.ForeColor, _
+                TextFormatFlags.HorizontalCenter Or TextFormatFlags.VerticalCenter)
+            '描画が完了したことを知らせる
+            e.Handled = True
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' 列ヘッダーダブルクリックイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub dgvList_ColumnHeaderMouseDoubleClick(sender As Object, e As System.Windows.Forms.DataGridViewCellMouseEventArgs) Handles dgvList.ColumnHeaderMouseDoubleClick
+        Dim targetColumn As DataGridViewColumn = dgvList.Columns(e.ColumnIndex) '選択列
+        dgvList.Sort(targetColumn, System.ComponentModel.ListSortDirection.Ascending) '昇順でソート
+    End Sub
+
+    ''' <summary>
+    ''' 対象の頭文字までスクロール
+    ''' </summary>
+    ''' <param name="initialChar">頭文字</param>
+    ''' <remarks></remarks>
+    Private Sub initialSearch(initialChar As String)
+        Dim rowsCount As Integer = dgvList.Rows.Count
+        For i As Integer = 0 To rowsCount - 1
+            Dim kana As String = Util.checkDBNullValue(dgvList("Kana", i).Value)
+            If System.Text.RegularExpressions.Regex.IsMatch(kana, "^" & initialChar) Then
+                dgvList.Rows(i).Selected = True
+                dgvList.FirstDisplayedScrollingRowIndex = i
+                Exit For
+            End If
+        Next
+    End Sub
+
+    ''' <summary>
+    ''' ｱ～ﾜボタンクリックイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub btnSearchKana_Click(sender As System.Object, e As System.EventArgs) Handles btnSearchA.Click, btnSearchKA.Click, btnSearchSA.Click, btnSearchTA.Click, btnSearchNA.Click, btnSearchHA.Click, btnSearchMA.Click, btnSearchYA.Click, btnSearchRA.Click, btnSearchWA.Click
+        Dim searchText As String = StrConv(sender.text, VbStrConv.Narrow)
+        initialSearch(searchText)
+    End Sub
+
 End Class

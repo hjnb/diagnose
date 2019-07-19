b470ac48e28243f35cd6017f766ab246cb25cd6f
DESKTOP-OJIT7DO\yoshi <yoshitaka7144@gmail.com>
2019-07-19 09:17:22 +0900

    基準値マスタ：作成

--- diagnose/diagnose/基準値マスタ.Designer.vb
+++ diagnose/diagnose/基準値マスタ.Designer.vb
@@ -22,8 +22,213 @@ Partial Class 基準値マスタ
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.namBox = New System.Windows.Forms.TextBox()
+        Me.low1Box = New System.Windows.Forms.TextBox()
+        Me.upp1Box = New System.Windows.Forms.TextBox()
+        Me.low2Box = New System.Windows.Forms.TextBox()
+        Me.upp2Box = New System.Windows.Forms.TextBox()
+        Me.btnRegist = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.dgvStdM = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvStdM, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Blue
+        Me.Label1.Location = New System.Drawing.Point(37, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(88, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "検査項目名"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label2.Location = New System.Drawing.Point(59, 75)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(77, 16)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "共通 （男）"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label3.Location = New System.Drawing.Point(96, 109)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(40, 16)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "（女）"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label4.Location = New System.Drawing.Point(153, 75)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(56, 16)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "下限値"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label5.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label5.Location = New System.Drawing.Point(153, 109)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(56, 16)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "下限値"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label6.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label6.Location = New System.Drawing.Point(338, 109)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(56, 16)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "上限値"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label7.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label7.Location = New System.Drawing.Point(338, 75)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(56, 16)
+        Me.Label7.TabIndex = 5
+        Me.Label7.Text = "上限値"
+        '
+        'namBox
+        '
+        Me.namBox.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.namBox.ImeMode = System.Windows.Forms.ImeMode.Hiragana
+        Me.namBox.Location = New System.Drawing.Point(134, 33)
+        Me.namBox.Name = "namBox"
+        Me.namBox.Size = New System.Drawing.Size(164, 23)
+        Me.namBox.TabIndex = 7
+        '
+        'low1Box
+        '
+        Me.low1Box.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.low1Box.ImeMode = System.Windows.Forms.ImeMode.Disable
+        Me.low1Box.Location = New System.Drawing.Point(217, 71)
+        Me.low1Box.Name = "low1Box"
+        Me.low1Box.Size = New System.Drawing.Size(107, 23)
+        Me.low1Box.TabIndex = 8
+        '
+        'upp1Box
+        '
+        Me.upp1Box.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.upp1Box.ImeMode = System.Windows.Forms.ImeMode.Disable
+        Me.upp1Box.Location = New System.Drawing.Point(401, 71)
+        Me.upp1Box.Name = "upp1Box"
+        Me.upp1Box.Size = New System.Drawing.Size(107, 23)
+        Me.upp1Box.TabIndex = 9
+        '
+        'low2Box
+        '
+        Me.low2Box.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.low2Box.ImeMode = System.Windows.Forms.ImeMode.Disable
+        Me.low2Box.Location = New System.Drawing.Point(217, 106)
+        Me.low2Box.Name = "low2Box"
+        Me.low2Box.Size = New System.Drawing.Size(107, 23)
+        Me.low2Box.TabIndex = 10
+        '
+        'upp2Box
+        '
+        Me.upp2Box.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.upp2Box.ImeMode = System.Windows.Forms.ImeMode.Disable
+        Me.upp2Box.Location = New System.Drawing.Point(401, 106)
+        Me.upp2Box.Name = "upp2Box"
+        Me.upp2Box.Size = New System.Drawing.Size(107, 23)
+        Me.upp2Box.TabIndex = 11
+        '
+        'btnRegist
+        '
+        Me.btnRegist.Location = New System.Drawing.Point(596, 94)
+        Me.btnRegist.Name = "btnRegist"
+        Me.btnRegist.Size = New System.Drawing.Size(75, 32)
+        Me.btnRegist.TabIndex = 12
+        Me.btnRegist.Text = "登録"
+        Me.btnRegist.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(670, 94)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(75, 32)
+        Me.btnDelete.TabIndex = 13
+        Me.btnDelete.Text = "削除"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'dgvStdM
+        '
+        Me.dgvStdM.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvStdM.Location = New System.Drawing.Point(74, 169)
+        Me.dgvStdM.Name = "dgvStdM"
+        Me.dgvStdM.RowTemplate.Height = 21
+        Me.dgvStdM.Size = New System.Drawing.Size(671, 481)
+        Me.dgvStdM.TabIndex = 14
+        '
+        '基準値マスタ
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(840, 661)
+        Me.Controls.Add(Me.dgvStdM)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnRegist)
+        Me.Controls.Add(Me.upp2Box)
+        Me.Controls.Add(Me.low2Box)
+        Me.Controls.Add(Me.upp1Box)
+        Me.Controls.Add(Me.low1Box)
+        Me.Controls.Add(Me.namBox)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "基準値マスタ"
         Me.Text = "基準値マスタ"
+        CType(Me.dgvStdM, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents namBox As System.Windows.Forms.TextBox
+    Friend WithEvents low1Box As System.Windows.Forms.TextBox
+    Friend WithEvents upp1Box As System.Windows.Forms.TextBox
+    Friend WithEvents low2Box As System.Windows.Forms.TextBox
+    Friend WithEvents upp2Box As System.Windows.Forms.TextBox
+    Friend WithEvents btnRegist As System.Windows.Forms.Button
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
+    Friend WithEvents dgvStdM As System.Windows.Forms.DataGridView
 End Class
--- diagnose/diagnose/基準値マスタ.vb
+++ diagnose/diagnose/基準値マスタ.vb
@@ -1,3 +1,322 @@
-﻿Public Class 基準値マスタ
+﻿Imports System.Data.OleDb
 
+Public Class 基準値マスタ
+
+    ''' <summary>
+    ''' 行ヘッダーのカレントセルを表す三角マークを非表示に設定する為のクラス。
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Class dgvRowHeaderCell
+
+        'DataGridViewRowHeaderCell を継承
+        Inherits DataGridViewRowHeaderCell
+
+        'DataGridViewHeaderCell.Paint をオーバーライドして行ヘッダーを描画
+        Protected Overrides Sub Paint(ByVal graphics As Graphics, ByVal clipBounds As Rectangle, _
+           ByVal cellBounds As Rectangle, ByVal rowIndex As Integer, ByVal cellState As DataGridViewElementStates, _
+           ByVal value As Object, ByVal formattedValue As Object, ByVal errorText As String, _
+           ByVal cellStyle As DataGridViewCellStyle, ByVal advancedBorderStyle As DataGridViewAdvancedBorderStyle, _
+           ByVal paintParts As DataGridViewPaintParts)
+            '標準セルの描画からセル内容の背景だけ除いた物を描画(-5)
+            MyBase.Paint(graphics, clipBounds, cellBounds, rowIndex, cellState, value, _
+                     formattedValue, errorText, cellStyle, advancedBorderStyle, _
+                     Not DataGridViewPaintParts.ContentBackground)
+        End Sub
+
+    End Class
+
+    ''' <summary>
+    ''' loadイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub 基準値マスタ_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Me.WindowState = FormWindowState.Maximized
+        Me.KeyPreview = True
+
+        initDgvStdM()
+        displayDgvStdM()
+    End Sub
+
+    ''' <summary>
+    ''' keyDownイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub 基準値マスタ_KeyDown(sender As Object, e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            If e.Control = False Then
+                Me.SelectNextControl(Me.ActiveControl, Not e.Shift, True, True, True)
+            End If
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' データグリッドビュー初期設定
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub initDgvStdM()
+        Util.EnableDoubleBuffering(dgvStdM)
+
+        With dgvStdM
+            .AllowUserToAddRows = False '行追加禁止
+            .AllowUserToResizeColumns = False '列の幅をユーザーが変更できないようにする
+            .AllowUserToResizeRows = False '行の高さをユーザーが変更できないようにする
+            .AllowUserToDeleteRows = False '行削除禁止
+            .BorderStyle = BorderStyle.FixedSingle
+            .MultiSelect = False
+            .SelectionMode = DataGridViewSelectionMode.FullRowSelect
+            .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft
+            .RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing
+            .ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+            .ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+            .RowHeadersWidth = 32
+            .RowTemplate.Height = 19
+            .RowTemplate.HeaderCell = New dgvRowHeaderCell() '行ヘッダの三角マークを非表示に
+            .BackgroundColor = Color.FromKnownColor(KnownColor.Control)
+            .ShowCellToolTips = False
+            .EnableHeadersVisualStyles = False
+            .Font = New Font("ＭＳ Ｐゴシック", 10)
+            .ReadOnly = True
+        End With
+    End Sub
+
+    ''' <summary>
+    ''' 入力内容クリア
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub clearInput()
+        namBox.Text = ""
+        low1Box.Text = ""
+        upp1Box.Text = ""
+        low2Box.Text = ""
+        upp2Box.Text = ""
+    End Sub
+
+    ''' <summary>
+    ''' データ表示
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub displayDgvStdM()
+        '内容クリア
+        dgvStdM.Columns.Clear()
+        namBox.Text = ""
+        low1Box.Text = ""
+        upp1Box.Text = ""
+        low2Box.Text = ""
+        upp2Box.Text = ""
+
+        'データ取得
+        Dim cnn As New ADODB.Connection
+        cnn.Open(topForm.DB_Diagnose)
+        Dim rs As New ADODB.Recordset
+        Dim sql As String = "select Nam, Low1, Upp1, Low2, Upp2 from StdM order by Nam"
+        rs.Open(sql, cnn, ADODB.CursorTypeEnum.adOpenForwardOnly, ADODB.LockTypeEnum.adLockReadOnly)
+        Dim da As OleDbDataAdapter = New OleDbDataAdapter()
+        Dim ds As DataSet = New DataSet()
+        da.Fill(ds, rs, "UsrM")
+        Dim dt As DataTable = ds.Tables("UsrM")
+
+        '表示
+        dgvStdM.DataSource = dt
+        If Not IsNothing(dgvStdM.CurrentRow) Then
+            dgvStdM.CurrentRow.Selected = False
+        End If
+
+        '幅設定等
+        With dgvStdM
+            With .Columns("Nam")
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft
+                .HeaderText = "検査項目名"
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+                .Width = 140
+            End With
+            With .Columns("Low1")
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+                .HeaderText = "共通（男）下限"
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+                .Width = 120
+            End With
+            With .Columns("Upp1")
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+                .HeaderText = "共通（男）上限"
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+                .Width = 120
+            End With
+            With .Columns("Low2")
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+                .HeaderText = "（女）下限"
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+                .Width = 120
+            End With
+            With .Columns("Upp2")
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+                .HeaderText = "（女）上限"
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+                .Width = 120
+            End With
+        End With
+
+    End Sub
+
+    ''' <summary>
+    ''' セルマウスクリックイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub dgvStdM_CellMouseClick(sender As Object, e As System.Windows.Forms.DataGridViewCellMouseEventArgs) Handles dgvStdM.CellMouseClick
+        If e.RowIndex >= 0 Then
+            Dim nam As String = Util.checkDBNullValue(dgvStdM("Nam", e.RowIndex).Value)
+            Dim low1 As String = Util.checkDBNullValue(dgvStdM("Low1", e.RowIndex).Value)
+            Dim upp1 As String = Util.checkDBNullValue(dgvStdM("Upp1", e.RowIndex).Value)
+            Dim low2 As String = Util.checkDBNullValue(dgvStdM("Low2", e.RowIndex).Value)
+            Dim upp2 As String = Util.checkDBNullValue(dgvStdM("Upp2", e.RowIndex).Value)
+
+            '各ボックスへ
+            namBox.Text = nam
+            low1Box.Text = low1
+            upp1Box.Text = upp1
+            low2Box.Text = low2
+            upp2Box.Text = upp2
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' CellPaintingイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub dgvStdM_CellPainting(sender As Object, e As System.Windows.Forms.DataGridViewCellPaintingEventArgs) Handles dgvStdM.CellPainting
+        '行ヘッダーかどうか調べる
+        If e.ColumnIndex < 0 AndAlso e.RowIndex >= 0 Then
+            'セルを描画する
+            e.Paint(e.ClipBounds, DataGridViewPaintParts.All)
+
+            '行番号を描画する範囲を決定する
+            'e.AdvancedBorderStyleやe.CellStyle.Paddingは無視しています
+            Dim indexRect As Rectangle = e.CellBounds
+            indexRect.Inflate(-2, -2)
+            '行番号を描画する
+            TextRenderer.DrawText(e.Graphics, _
+                (e.RowIndex + 1).ToString(), _
+                e.CellStyle.Font, _
+                indexRect, _
+                e.CellStyle.ForeColor, _
+                TextFormatFlags.HorizontalCenter Or TextFormatFlags.VerticalCenter)
+            '描画が完了したことを知らせる
+            e.Handled = True
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' 登録ボタンクリックイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub btnRegist_Click(sender As System.Object, e As System.EventArgs) Handles btnRegist.Click
+        '検査項目名
+        Dim nam As String = namBox.Text
+        If nam = "" Then
+            MsgBox("検査項目名を入力して下さい。", MsgBoxStyle.Exclamation)
+            namBox.Focus()
+            Return
+        End If
+        '共通（男）下限
+        Dim low1 As String = low1Box.Text
+        If Not System.Text.RegularExpressions.Regex.IsMatch(low1, "^\d+(\.\d+)?$") Then
+            MsgBox("数値を入力して下さい。", MsgBoxStyle.Exclamation)
+            low1Box.Focus()
+            Return
+        End If
+        '共通（男）上限
+        Dim upp1 As String = upp1Box.Text
+        If Not System.Text.RegularExpressions.Regex.IsMatch(upp1, "^\d+(\.\d+)?$") Then
+            MsgBox("数値を入力して下さい。", MsgBoxStyle.Exclamation)
+            upp1Box.Focus()
+            Return
+        End If
+        '共通（女）下限
+        Dim low2 As String = low2Box.Text
+        If Not System.Text.RegularExpressions.Regex.IsMatch(low2, "^\d+(\.\d+)?$") Then
+            MsgBox("数値を入力して下さい。", MsgBoxStyle.Exclamation)
+            low2Box.Focus()
+            Return
+        End If
+        '共通（女）上限
+        Dim upp2 As String = upp2Box.Text
+        If Not System.Text.RegularExpressions.Regex.IsMatch(upp2, "^\d+(\.\d+)?$") Then
+            MsgBox("数値を入力して下さい。", MsgBoxStyle.Exclamation)
+            upp2Box.Focus()
+            Return
+        End If
+
+        '登録
+        Dim cn As New ADODB.Connection()
+        cn.Open(topForm.DB_Diagnose)
+        Dim sql As String = "select * from StdM where Nam = '" & nam & "'"
+        Dim rs As New ADODB.Recordset
+        rs.Open(sql, cn, ADODB.CursorTypeEnum.adOpenKeyset, ADODB.LockTypeEnum.adLockOptimistic)
+        If rs.RecordCount <= 0 Then
+            rs.AddNew()
+            rs.Fields("Nam").Value = nam
+        End If
+        rs.Fields("Low1").Value = low1
+        rs.Fields("Upp1").Value = upp1
+        rs.Fields("Low2").Value = low2
+        rs.Fields("Upp2").Value = upp2
+        rs.Update()
+        rs.Close()
+        cn.Close()
+
+        '再表示
+        displayDgvStdM()
+    End Sub
+
+    ''' <summary>
+    ''' 削除ボタンクリックイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub btnDelete_Click(sender As System.Object, e As System.EventArgs) Handles btnDelete.Click
+        '検査項目名
+        Dim nam As String = namBox.Text
+        If nam = "" Then
+            MsgBox("検査項目名を入力して下さい。", MsgBoxStyle.Exclamation)
+            namBox.Focus()
+            Return
+        End If
+
+        '存在チェック
+        Dim cn As New ADODB.Connection()
+        cn.Open(topForm.DB_Diagnose)
+        Dim sql As String = "select * from StdM where Nam = '" & nam & "'"
+        Dim rs As New ADODB.Recordset
+        rs.Open(sql, cn, ADODB.CursorTypeEnum.adOpenKeyset, ADODB.LockTypeEnum.adLockOptimistic)
+        If rs.RecordCount <= 0 Then
+            MsgBox("データが存在しません。", MsgBoxStyle.Exclamation)
+            rs.Close()
+            cn.Close()
+            Return
+        Else
+            Dim result As DialogResult = MessageBox.Show("削除してよろしいですか？", "削除", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+            If result = Windows.Forms.DialogResult.Yes Then
+                rs.Delete()
+                rs.Update()
+                rs.Close()
+                cn.Close()
+
+                '再表示
+                displayDgvStdM()
+            Else
+                rs.Close()
+                cn.Close()
+            End If
+        End If
+    End Sub
 End Class
